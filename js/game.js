f9dfc58654d4e34db4cefc1937241ba66f618463
satzch <gamingsplay240@gmail.com>
2024-08-01 23:31:42 +0530

    add grid to screen for debug purpose

--- js/game.js
+++ js/game.js
@@ -1,10 +1,21 @@
 let continueGame = true;
+// continueGame = false;
 function gameLoop()
 {
     // clear screen
     ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    for (let i = 0; i < SCREEN_WIDTH; i++ )
+        {
+            for (let j = 0; j < SCREEN_HEIGHT; j++)
+            {
+                ctx.strokeRect(i*20, j*20, 20, 20);
+            }
+        }
     snake.update();
     snake.draw();
+    // food.update();
+    food.checkCollision();
+    food.draw();
     if (continueGame)
     {
         setTimeout(gameLoop, 500);
