499b3baaef789d2a7e9967463f2b50cefd82e266
satzch <gamingsplay240@gmail.com>
2024-08-01 22:03:02 +0530

    add snake head as js object

--- js/setup.js
+++ js/setup.js
@@ -2,7 +2,40 @@
 const canvas = document.getElementById("canvas");
 
 const ctx = canvas.getContext("2d");
-canvas.width = 480;
-canvas.height = 360;
 
-ctx.fillRect(100, 100, 200, 200);
+const SCREEN_WIDTH = 480;
+const SCREEN_HEIGHT = 360;
+canvas.width = SCREEN_WIDTH;
+canvas.height = SCREEN_HEIGHT;
+
+
+const snake = {
+    pos: {
+        x: Math.random() * SCREEN_WIDTH/2 + SCREEN_WIDTH/4,
+        y: Math.random() * SCREEN_HEIGHT/2 + SCREEN_HEIGHT/4
+    },
+    vel: {
+        x: 2,
+        y: 2
+    },
+    size: 20,
+    color: "rgb(255, 100, 200)",
+    setPos(pos_x, pos_y) {
+        this.pos.x = pos_x;
+        this.pos.y = pos_y;
+    },
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(
+            this.pos.x-(this.size/2),
+            this.pos.y-(this.size/2),
+            this.size/2,
+            this.size/2
+        );
+    },
+    update() {
+        this.pos.x += this.vel.x;
+        this.pos.y += this.vel.y;
+    }
+};
+snake.draw();
