6ef18ff89ab92c7d17bb5dddc5645f41022bf901
satzch <gamingsplay240@gmail.com>
2024-08-03 23:27:09 +0530

    add: game over state

--- js/game.js
+++ js/game.js
@@ -1,7 +1,9 @@
 let continueGame = true;
 // continueGame = false;
+let gameOver = false;
 function gameLoop()
 {
+    // if (gameOver) return;
     // clear screen
     ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
     for (let i = 0; i < SCREEN_WIDTH; i++ )
@@ -11,15 +13,16 @@ function gameLoop()
             ctx.strokeRect(i*20, j*20, 20, 20);
         }
     }
-    snake.update();
+    if (!gameOver) snake.update();
     let temp = snake;
     temp.draw();
     while (temp.next != null) {
         temp = temp.next;
-        temp.update();
+        if (!gameOver) temp.update();
         temp.draw();
     }
     snake.draw();
+    gameOver = checkSnakeBodyCollision(snake);
     food.checkCollision();
     food.draw();
     if (continueGame)
