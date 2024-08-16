cc9f3b6334ef61d732078cae7584e7708fa31421
satzch <gamingsplay240@gmail.com>
2024-08-16 13:14:46 +0530

    add useful comments

--- js/smartphone_check.js
+++ js/smartphone_check.js
@@ -10,31 +10,38 @@ function isSmartphone() {
     return (isTouchDevice && isSmartphoneUA)
 }
 
-
+// required dom elements for smartphone controls
 const rotate_screen = document.getElementById("rotate-smartphone");
 const dpad = document.getElementById("ui-controls-dpad");
 const ui_keyboard_instructions = document.getElementById("ui-controls-box");
 
+// checks if device is smartphone
+// shows and hides dpad controls
 function toggleSmartphoneMode() {
     if (isSmartphone()) {
         rotate_screen.classList.remove("hide");
         dpad.classList.remove("hide");
         ui_keyboard_instructions.classList.add("hide");
+        // add event listeners to dpad if device is smartphone
+        // otherwise it will be hidden anyways
+        addDpadListeners();
     } else {
         rotate_screen.classList.add("hide");
         dpad.classList.add("hide");
         ui_keyboard_instructions.classList.remove("hide");
     }
-    addDpadListeners();
 }
 
+// call once initially when the site is loaded
 toggleSmartphoneMode();
 
+// check if user is using some kind of emulator for different devices
+// and toggle the smartphone mode
 window.addEventListener("resize", () => {
     toggleSmartphoneMode();
 })
 
-
+// Add event listeners to the dpad controls that will show in smartphones
 function addDpadListeners() {
     const dpad_up = document.getElementById("dpad-up");
     const dpad_down = document.getElementById("dpad-down");
@@ -42,15 +49,19 @@ function addDpadListeners() {
     const dpad_right = document.getElementById("dpad-right");
 
     dpad_up.addEventListener("click", () => {
+        // pass fake event obj to already implemented eventHandler to reduce redundancy
         debounceKeyHandler({key: "ArrowUp"});
     });
     dpad_down.addEventListener("click", () => {
+        // pass fake event obj to already implemented eventHandler to reduce redundancy
         debounceKeyHandler({key: "ArrowDown"});
     });
     dpad_left.addEventListener("click", () => {
+        // pass fake event obj to already implemented eventHandler to reduce redundancy
         debounceKeyHandler({key: "ArrowLeft"});
     });
     dpad_right.addEventListener("click", () => {
+        // pass fake event obj to already implemented eventHandler to reduce redundancy
         debounceKeyHandler({key: "ArrowRight"});
     });
 }
