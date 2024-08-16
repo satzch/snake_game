2dd80c1ae929eb2ef2d0084859ce2b0436d8ea9d
satzch <gamingsplay240@gmail.com>
2024-08-16 12:39:47 +0530

    add base dpad controls for smartphones

--- js/smartphone_check.js
+++ js/smartphone_check.js
@@ -1,6 +1,6 @@
 // checks if the device is smartphone by checking userAgent
 function isSmartphone() {
-    const isSmartphoneUA = /android|iPhone|iPod/.test(navigator.userAgent);
+    const isSmartphoneUA = /android|Android|iPhone|iPod|iPad/.test(navigator.userAgent);
     
     const isTouchDevice = 
         'ontouchstart' in window ||
@@ -9,6 +9,25 @@ function isSmartphone() {
     return (isTouchDevice && isSmartphoneUA)
 }
 
-if (isSmartphone()) {
-    
+
+const rotate_screen = document.getElementById("rotate-smartphone");
+const dpad = document.getElementById("ui-controls-dpad");
+const ui_keyboard_instructions = document.getElementById("ui-controls-box");
+
+function toggleSmartphoneMode() {
+    if (isSmartphone()) {
+        rotate_screen.classList.remove("hide");
+        dpad.classList.remove("hide");
+        ui_keyboard_instructions.classList.add("hide");
+    } else {
+        rotate_screen.classList.add("hide");
+        dpad.classList.add("hide");
+        ui_keyboard_instructions.classList.remove("hide");
+    }
 }
+
+toggleSmartphoneMode();
+
+window.addEventListener("resize", () => {
+    toggleSmartphoneMode();
+})
