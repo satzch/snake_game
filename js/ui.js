e2681467c53ed23584852175800d6ed6f9fab617
satzch <gamingsplay240@gmail.com>
2024-11-29 16:16:50 +0530

    add more correct looking loading bar

--- js/ui.js
+++ js/ui.js
@@ -124,49 +124,97 @@ console.log(performance)
 
 // loading screen
 
-window.onload = function() {
-    const loading_screen = document.getElementById("loading-screen");
-    const progress_bar = document.getElementById("loading-progress-bar");
-    const loading_percent_text = document.getElementById("loading-percent-text");
+// window.onload = function() {
+//     const loading_screen = document.getElementById("loading-screen");
+//     const progress_bar = document.getElementById("loading-progress-bar");
+//     const loading_percent_text = document.getElementById("loading-percent-text");
 
 
-    // Simulate loading progress based on performance data
-    let updateProgress = function() {
-        let totalResources = performance.getEntriesByType("resource").length;
-        let loadedResources = 0;
+//     // Simulate loading progress based on performance data
+//     let updateProgress = function() {
+//         let totalResources = performance.getEntriesByType("resource").length;
+//         let loadedResources = 0;
 
-        // Count resources that have finished loading
-        performance.getEntriesByType("resource").forEach((resource) => {
-            if (resource.responseStart > 0) {
-                loadedResources++;
-            }
-        });
+//         // Count resources that have finished loading
+//         performance.getEntriesByType("resource").forEach((resource) => {
+//             if (resource.responseStart > 0) {
+//                 loadedResources++;
+//             }
+//         });
 
-        // Calculate the percentage of resources loaded
-        let percent = Math.round((loadedResources / totalResources) * 100);
+//         // Calculate the percentage of resources loaded
+//         let percent = Math.round((loadedResources / totalResources) * 100);
 
-        progress_bar.style.width = percent + '%';
-        loading_percent_text.innerHTML = percent + '%';
+//         progress_bar.style.width = percent + '%';
+//         loading_percent_text.innerHTML = percent + '%';
 
-        // If all resources are loaded, hide the loading screen
-        if (percent >= 100) {
-            percent = 100;
-            setTimeout(() => {
-                loading_screen.classList.add("hide");
-            }, 500);
-        }
-    };
+//         // If all resources are loaded, hide the loading screen
+//         if (percent >= 100) {
+//             percent = 100;
+//             setTimeout(() => {
+//                 loading_screen.classList.add("hide");
+//             }, 500);
+//         }
+//     };
 
-    // Update progress every 10ms
-    let interval = setInterval(() => {
-        updateProgress();
-        if (performance.timing.loadEventEnd > 0) {
-            clearInterval(interval);
-        }
-    }, 10);
+//     // Update progress every 10ms
+//     let interval = setInterval(() => {
+//         updateProgress();
+//         if (performance.timing.loadEventEnd > 0) {
+//             clearInterval(interval);
+//         }
+//     }, 10);
 
-    updateProgress();
-};
+//     updateProgress();
+// };
+
+
+const loading_screen = document.getElementById("loading-screen");
+const progress_bar = document.getElementById("loading-progress-bar");
+const loading_percent_text = document.getElementById("loading-percent-text");
+
+// This function simulates progress based on performance timings
+let progress = 0;
+function simulateProgress() {
+    const performance_timing = performance.timing;
+    
+    if (performance_timing.loadEventEnd > 0 && performance_timing.navigationStart > 0) {
+        let total_time = performance_timing.loadEventEnd - performance_timing.navigationStart;
+        let current_time = performance_timing.domContentLoadedEventEnd - performance_timing.navigationStart;
+
+        if (total_time > 0) {
+            progress = (current_time / total_time) * 100;
+        } 
+            
+    } else {
+        // increase the progress until actual results are calculated
+        progress += 0.1;
+        if (progress > 50) progress = 50;
+    }
+    
+    if (progress > 100) progress = 100;
+    updateProgressBar(progress);
+}
+
+function updateProgressBar(progress) {
+    progress_bar.style.width = progress + '%';
+    loading_percent_text.innerHTML = Math.round(progress) + '%';
+    console.log(progress)
+    console.log(document.readyState)
+}
+
+// Update progress bar periodically
+const interval = setInterval(function() {
+    simulateProgress();
+    // Stop when the page is fully loaded
+    if (document.readyState === 'complete') {
+        clearInterval(interval);
+        setTimeout(function() {
+            loading_screen.classList.add("hide");
+        }, 1000);
+    }
+}, 10); // Update every 10ms
+
 
 
 // restart keyboard key
